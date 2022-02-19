e2035f4c64199f2e9d41241413942a78d9092d27
Roma_xs97 <Roma_xs97@student.itstep.org>
2022-02-19 20:52:16 +0200

    Operations final

---
+++ Operations/Operations/Exceptions/ComplexExceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations.Exceptions
+{
+    internal class ComplexExceptions : Exception
+    {
+        public ComplexExceptions() : base("Invalid operation with complex number(s)")
+        {
+
+        }
+        public ComplexExceptions(string message):base(message)
+        {
+
+        }
+    }
+}
